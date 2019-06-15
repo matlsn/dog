ee9b6dd4b5fe7afa75ec833fe229e816cf9e41ec
mat <32943178+mn6@users.noreply.github.com>
2019-06-15 17:35:12 -0400

    Integrate new version of commitscrape

--- assets/main.js
+++ assets/main.js
@@ -1,32 +1,11 @@
 $.get('https://commits.mat.dog/', function (res) {
   if (!res || res.length < 10) return
   let prep = res
-  prep = prep.slice(prep.indexOf('<g transform="translate(420'))
-  prep = '<svg class="js-calendar-graph-svg" width="285" height="113"><g transform="translate(5, 20)">' + prep
-  prep = $(prep).find('g g').each(function (i, e) {
-    $(e).attr('transform', `translate(${14*i}, 0)`)
-  }).end().find('rect').each(function (_, e) {
+  prep = $(prep).find('rect').each(function (_, e) {
     const dcount =  +$(e).attr('data-count')
     const contr = dcount === 0 ? 'No contributions' : dcount === 1 ? '1 contribution' : `${dcount} contributions`
     const day = new Date($(e).attr('data-date')).toDateString().substr(4)
     $(e).attr('aria-label', `${contr} on ${day}`)
-    
-    var clr = $(e).attr('fill')
-    function change (col) { $(e).attr('fill', col) }
-    switch (clr) {
-      case '#ebedf0':
-        change('#69c7f1')
-        break
-      case '#196127':
-        change('#115371')
-        break
-      case '#c6e48b':
-        change('#1d82af')
-        break
-      case '#7bc96f':
-        change('#2699cc')
-        break
-    }
   }).end()
   $('#commits').html(prep)
   $('.content').addClass('loaded')

16494834152e425e10c515fa1be2e06987aaa3a4
mat <mathiaslsn2@gmail.com>
2019-06-16 03:35:13 +0000

    Integrating new version of commitscrape

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,6 @@
 $.get('https://commits.mat.dog/', function (res) {
   if (!res || res.length < 10) return
   let prep = res
-  prep = $(prep).find('rect').each(function (_, e) {
-    const dcount =  +$(e).attr('data-count')
-    const contr = dcount === 0 ? 'No contributions' : dcount === 1 ? '1 contribution' : `${dcount} contributions`
-    const day = new Date($(e).attr('data-date')).toDateString().substr(4)
-    $(e).attr('aria-label', `${contr} on ${day}`)
-  }).end()
   $('#commits').html(prep)
   $('.content').addClass('loaded')
 })
@@ -17,7 +11,7 @@ const doTipsy = cont => {
   $('.links').after(tipsy.replace('%s', cont.text).replace('%l', cont.coords.left).replace('%t', cont.coords.top));
 }
 
-$(document).on('mouseenter', 'rect', e => {
+$(document).on('mouseenter', '.commitscrape-block', e => {
   let offset = $(e.currentTarget).offset()
   offset.top = offset.top - 409
   offset.left = offset.left - 120
@@ -25,6 +19,6 @@ $(document).on('mouseenter', 'rect', e => {
     text: $(e.currentTarget).attr('aria-label'),
     coords: offset
   })
-}).on('mouseleave', 'rect', e => {
+}).on('mouseleave', '.commitscrape-block', e => {
   $('.tipsy').remove()
 })
